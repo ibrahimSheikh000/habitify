c8efdab8c565cd64936fbc70af28ffb3315ef7ce
Ibrahim Ahmed <ibrahimahmed000@gmail.com>
2024-09-26 15:35:14 +0500

    feat apidocs

---
+++ script.js
@@ -0,0 +1,13 @@
+const main = `  <div class="col-md-8">
+                <main>
+                    <h2>Getting Started</h2>
+                    <p>This is the official documentation for Habitify API. A reference to the functionality our public API provides with detailed descriptions of each API endpoint, parameters, and examples.</p>
+                    
+                    <p>Habitify API is organized around <a href="#">REST</a>. Our API has predictable resource-oriented URLs, returns <a href="#">JSON-encoded</a> responses, and uses standard HTTP response codes, authentication, and verbs.</p>
+                    
+                    <p>To get started, you should continue with the <a href="#">Authentication</a> to learn all the details on the best way to authenticate to our server.</p>
+                    
+                    <h2>Base URL</h2>
+                    <code>https://api.habitify.me</code>`
+
+document.getElementById("main").innerHTML = maincode;
